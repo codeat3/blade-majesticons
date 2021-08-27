52d27c91f506ae236458ec3be44db54134c6ca53
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-08-27 13:01:38 +0530

    updated to phpcsfixer 3

--- src/BladeMajesticIconsServiceProvider.php
+++ src/BladeMajesticIconsServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeMajesticIconsServiceProvider extends ServiceProvider
 
             $factory->add('majestic-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
@@ -39,5 +38,4 @@ final class BladeMajesticIconsServiceProvider extends ServiceProvider
             ], 'blade-majestic-icons-config');
         }
     }
-
 }
