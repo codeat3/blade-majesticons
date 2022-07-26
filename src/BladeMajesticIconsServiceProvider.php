d856054025b1ba39020334685bc7d3d6b3f75b59
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:14:30 +0000

    Fix styling

--- src/BladeMajesticIconsServiceProvider.php
+++ src/BladeMajesticIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeMajesticIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-majestic-icons', []);
 
-            $factory->add('majestic-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('majestic-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-majestic-icons.php', 'blade-majestic-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-majestic-icons.php', 'blade-majestic-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-majestic-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-majestic-icons'),
             ], 'blade-majestic-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-majestic-icons.php' => $this->app->configPath('blade-majestic-icons.php'),
+                __DIR__ . '/../config/blade-majestic-icons.php' => $this->app->configPath('blade-majestic-icons.php'),
             ], 'blade-majestic-icons-config');
         }
     }
