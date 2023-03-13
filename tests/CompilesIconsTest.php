71de0ca5229f431768bbb7dc302878529c5611ff
Swapnil Sarwe <swapnilsarwe@gmail.com>
2023-03-13 12:42:11 +0530

    updated read me with correct icons

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -14,11 +14,11 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_compiles_a_single_anonymous_component()
     {
-        $result = svg('majestic-academic-cap-line')->toHtml();
+        $result = svg('majestic-cookie-line')->toHtml();
 
         // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<'SVG'
-            <svg fill="currentColor" viewBox="0 0 24 24"><path d="M12.486 5.414a1 1 0 00-.972 0L5.06 9l6.455 3.586a1 1 0 00.972 0L18.94 9l-6.455-3.586zm-1.943-1.749a3 3 0 012.914 0l8.029 4.46a1 1 0 010 1.75l-8.03 4.46a3 3 0 01-2.913 0l-8.029-4.46a1 1 0 010-1.75l8.03-4.46z"/><path d="M21 8a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zM6 10a1 1 0 011 1v5.382l4.553 2.276a1 1 0 00.894 0L17 16.382V11a1 1 0 112 0v6a1 1 0 01-.553.894l-5.105 2.553a3 3 0 01-2.684 0l-5.105-2.553A1 1 0 015 17v-6a1 1 0 011-1z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.419 12c0-6.4 5.358-8 8.037-8h1.005v4h3.014v3h4.019v1c0 6.4-5.024 8-8.038 8-3.014 0-8.037-1.6-8.037-8zm5.023-4h.001m-2.01 4h0m5.023 1h.001m-2.01 3h0m6.028-1h0"/></svg>
             SVG;
 
 
@@ -28,9 +28,9 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_classes_to_icons()
     {
-        $result = svg('majestic-academic-cap-line', 'w-6 h-6 text-gray-500')->toHtml();
+        $result = svg('majestic-cookie-line', 'w-6 h-6 text-gray-500')->toHtml();
         $expected = <<<'SVG'
-            <svg class="w-6 h-6 text-gray-500" fill="currentColor" viewBox="0 0 24 24"><path d="M12.486 5.414a1 1 0 00-.972 0L5.06 9l6.455 3.586a1 1 0 00.972 0L18.94 9l-6.455-3.586zm-1.943-1.749a3 3 0 012.914 0l8.029 4.46a1 1 0 010 1.75l-8.03 4.46a3 3 0 01-2.913 0l-8.029-4.46a1 1 0 010-1.75l8.03-4.46z"/><path d="M21 8a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zM6 10a1 1 0 011 1v5.382l4.553 2.276a1 1 0 00.894 0L17 16.382V11a1 1 0 112 0v6a1 1 0 01-.553.894l-5.105 2.553a3 3 0 01-2.684 0l-5.105-2.553A1 1 0 015 17v-6a1 1 0 011-1z"/></svg>
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.419 12c0-6.4 5.358-8 8.037-8h1.005v4h3.014v3h4.019v1c0 6.4-5.024 8-8.038 8-3.014 0-8.037-1.6-8.037-8zm5.023-4h.001m-2.01 4h0m5.023 1h.001m-2.01 3h0m6.028-1h0"/></svg>
             SVG;
         $this->assertSame($expected, $result);
     }
@@ -38,11 +38,11 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_styles_to_icons()
     {
-        $result = svg('majestic-academic-cap-line', ['style' => 'color: #555'])->toHtml();
+        $result = svg('majestic-cookie-line', ['style' => 'color: #555'])->toHtml();
 
 
         $expected = <<<'SVG'
-            <svg style="color: #555" fill="currentColor" viewBox="0 0 24 24"><path d="M12.486 5.414a1 1 0 00-.972 0L5.06 9l6.455 3.586a1 1 0 00.972 0L18.94 9l-6.455-3.586zm-1.943-1.749a3 3 0 012.914 0l8.029 4.46a1 1 0 010 1.75l-8.03 4.46a3 3 0 01-2.913 0l-8.029-4.46a1 1 0 010-1.75l8.03-4.46z"/><path d="M21 8a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zM6 10a1 1 0 011 1v5.382l4.553 2.276a1 1 0 00.894 0L17 16.382V11a1 1 0 112 0v6a1 1 0 01-.553.894l-5.105 2.553a3 3 0 01-2.684 0l-5.105-2.553A1 1 0 015 17v-6a1 1 0 011-1z"/></svg>
+            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.419 12c0-6.4 5.358-8 8.037-8h1.005v4h3.014v3h4.019v1c0 6.4-5.024 8-8.038 8-3.014 0-8.037-1.6-8.037-8zm5.023-4h.001m-2.01 4h0m5.023 1h.001m-2.01 3h0m6.028-1h0"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -53,10 +53,10 @@ class CompilesIconsTest extends TestCase
     {
         Config::set('blade-majestic-icons.class', 'awesome');
 
-        $result = svg('majestic-academic-cap-line')->toHtml();
+        $result = svg('majestic-cookie-line')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="awesome" fill="currentColor" viewBox="0 0 24 24"><path d="M12.486 5.414a1 1 0 00-.972 0L5.06 9l6.455 3.586a1 1 0 00.972 0L18.94 9l-6.455-3.586zm-1.943-1.749a3 3 0 012.914 0l8.029 4.46a1 1 0 010 1.75l-8.03 4.46a3 3 0 01-2.913 0l-8.029-4.46a1 1 0 010-1.75l8.03-4.46z"/><path d="M21 8a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zM6 10a1 1 0 011 1v5.382l4.553 2.276a1 1 0 00.894 0L17 16.382V11a1 1 0 112 0v6a1 1 0 01-.553.894l-5.105 2.553a3 3 0 01-2.684 0l-5.105-2.553A1 1 0 015 17v-6a1 1 0 011-1z"/></svg>
+            <svg class="awesome" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.419 12c0-6.4 5.358-8 8.037-8h1.005v4h3.014v3h4.019v1c0 6.4-5.024 8-8.038 8-3.014 0-8.037-1.6-8.037-8zm5.023-4h.001m-2.01 4h0m5.023 1h.001m-2.01 3h0m6.028-1h0"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -68,10 +68,10 @@ class CompilesIconsTest extends TestCase
     {
         Config::set('blade-majestic-icons.class', 'awesome');
 
-        $result = svg('majestic-academic-cap-line', 'w-6 h-6')->toHtml();
+        $result = svg('majestic-cookie-line', 'w-6 h-6')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="awesome w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12.486 5.414a1 1 0 00-.972 0L5.06 9l6.455 3.586a1 1 0 00.972 0L18.94 9l-6.455-3.586zm-1.943-1.749a3 3 0 012.914 0l8.029 4.46a1 1 0 010 1.75l-8.03 4.46a3 3 0 01-2.913 0l-8.029-4.46a1 1 0 010-1.75l8.03-4.46z"/><path d="M21 8a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zM6 10a1 1 0 011 1v5.382l4.553 2.276a1 1 0 00.894 0L17 16.382V11a1 1 0 112 0v6a1 1 0 01-.553.894l-5.105 2.553a3 3 0 01-2.684 0l-5.105-2.553A1 1 0 015 17v-6a1 1 0 011-1z"/></svg>
+            <svg class="awesome w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.419 12c0-6.4 5.358-8 8.037-8h1.005v4h3.014v3h4.019v1c0 6.4-5.024 8-8.038 8-3.014 0-8.037-1.6-8.037-8zm5.023-4h.001m-2.01 4h0m5.023 1h.001m-2.01 3h0m6.028-1h0"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
